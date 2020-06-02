f0b20e7220f54734226d2b2f97837144ee028289
Alexander <ache167@gmail.com>
2020-06-02 04:13:13 +0300

    done

--- 04_fractal.py
+++ 04_fractal.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
 # - точка начала рисования,
@@ -26,16 +27,67 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
+def draw_branches(start_point, angle, length):
+    if length < 3:
+        return
+
+    next_angle_right_branch = angle - 30
+    next_angle_left_branch = angle + 30
+
+    length_factor = 0.75
+
+    left_branch = sd.get_vector(start_point=start_point, angle=next_angle_left_branch, length=length * length_factor)
+    left_branch.draw()
+    right_branch = sd.get_vector(start_point=start_point, angle=next_angle_right_branch, length=length * length_factor)
+    right_branch.draw()
+
+    draw_branches(start_point=right_branch.end_point, angle=next_angle_right_branch, length=length * length_factor)
+    draw_branches(start_point=left_branch.end_point, angle=next_angle_left_branch, length=length * length_factor)
+
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
-# - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
 # Возможный результат решения см lesson_004/results/exercise_04_fractal_02.jpg
+# - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
+
+def draw_branches_randomized(start_point, angle, length):
+    if length < 3:
+        return
+
+    angle_range_left = int(0.6 * 30)
+    angle_range_right = int(1.4 * 30)
+    angle_random_change_left = sd.randint(angle_range_left, angle_range_right)
+    angle_random_change_right = sd.randint(angle_range_left, angle_range_right)
+    next_angle_left_branch = angle + angle_random_change_left
+    next_angle_right_branch = angle - angle_random_change_right
+
+    length_range_left = int(0.8 * 0.75 * length)
+    length_range_right = int(1.2 * 0.75 * length)
+    length_left_rand_factor = sd.randint(length_range_left, length_range_right)
+    length_right_rand_factor = sd.randint(length_range_left, length_range_right)
+
+    left_branch = sd.get_vector(start_point=start_point,
+                                angle=next_angle_left_branch, length=length_left_rand_factor)
+    left_branch.draw()
+
+    right_branch = sd.get_vector(start_point=start_point, angle=next_angle_right_branch,
+                                 length=length_right_rand_factor)
+    right_branch.draw()
+
+    draw_branches_randomized(start_point=left_branch.end_point, angle=next_angle_left_branch,
+                             length=length_left_rand_factor)
+    draw_branches_randomized(start_point=right_branch.end_point, angle=next_angle_right_branch,
+                             length=length_right_rand_factor)
+
+
+ground_point_start = sd.get_point(300, 0)
+ground_point_end = sd.get_point(300, 30)
+sd.line(start_point=ground_point_start, end_point=ground_point_end)
+
+root_point = sd.get_point(300, 30)
+draw_branches_randomized(start_point=root_point, angle=90, length=100)
 
 # Пригодятся функции
 # sd.random_number()
 
 sd.pause()
-
-
--- 05_snowfall.py
+++ 05_snowfall.py
@@ -1,14 +1,33 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
-
+import random
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+N = 40
+x_screen_res, y_screen_res = 1200, 800
+sd.resolution = (x_screen_res, y_screen_res)
+y_top_half = int(y_screen_res / 2) + 200
+# нарисовать снежинку в точке center с длинной лучей length цветом color
+#         factor_a - место ответвления лучиков factor_a=0.6
+#         factor_b - длина лучиков factor_b=0.35
+#         factor_c - угол отклонения лучиков factor_c=60
+snowflakes_starting_points = []
+snowflakes_lengths = []
+snowflakes_factors_a_b_c = []
+snowflakes_fallen_coordinates = []
+snowflakes_fallen_lengths = []
+snowflakes_fallen_factors_a_b_c = []
+
+for i in range(N):
+    snowflakes_starting_points.append([sd.randint(0, x_screen_res), sd.randint(y_top_half, y_screen_res + 450)])
+    snowflakes_lengths.append(sd.randint(10, 50))
+    snowflakes_factors_a_b_c.append(
+        [round(random.uniform(0.3, 0.9), 2), round(random.uniform(0.15, 0.55), 2), round(sd.randint(50, 70), 2)])
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,31 +36,56 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
 while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
+    for i in range(len(snowflakes_starting_points)):
+        point = sd.get_point(snowflakes_starting_points[i][0], snowflakes_starting_points[i][1])
+        sd.start_drawing()
+        sd.snowflake(center=point, length=snowflakes_lengths[i], factor_a=snowflakes_factors_a_b_c[i][0],
+                     factor_b=snowflakes_factors_a_b_c[i][1], factor_c=snowflakes_factors_a_b_c[i][2],
+                     color=sd.background_color)
+
+        if snowflakes_starting_points[i][1] >= snowflakes_lengths[i]:
+            snowflakes_starting_points[i][1] -= sd.randint(12, 40)
+            left_or_right_rand = round(random.uniform(0, 1), 2)
+            if left_or_right_rand < 0.5:
+                snowflakes_starting_points[i][0] -= sd.randint(3, 10)
+            else:
+                snowflakes_starting_points[i][0] += sd.randint(3, 10)
+
+            point2 = sd.get_point(snowflakes_starting_points[i][0], snowflakes_starting_points[i][1])
+
+            sd.snowflake(center=point2, length=snowflakes_lengths[i], factor_a=snowflakes_factors_a_b_c[i][0],
+                         factor_b=snowflakes_factors_a_b_c[i][1], factor_c=snowflakes_factors_a_b_c[i][2],
+                         color=sd.COLOR_WHITE)
+        else:
+            # adding snowflake that fallen into a ground to fallen lists
+            snowflakes_fallen_coordinates.append([snowflakes_starting_points[i][0], snowflakes_starting_points[i][1]])
+            snowflakes_fallen_factors_a_b_c.append(
+                [snowflakes_factors_a_b_c[i][0], snowflakes_factors_a_b_c[i][1], snowflakes_factors_a_b_c[i][2]])
+            snowflakes_fallen_lengths.append(snowflakes_lengths[i])
+
+            # removing a fallen snowflake from list of falling snowflakes
+            snowflakes_starting_points.pop(i)
+            snowflakes_lengths.pop(i)
+            snowflakes_factors_a_b_c.pop(i)
+
+            # creating a new snowflake in a list of falling snowflakes
+            snowflakes_lengths.append(sd.randint(10, 50))
+            snowflakes_starting_points.append([sd.randint(0, x_screen_res), y_screen_res + 50])
+            snowflakes_factors_a_b_c.append(
+                [round(random.uniform(0.3, 0.9), 2), round(random.uniform(0.15, 0.55), 2),
+                 round(sd.randint(50, 70), 2)])
+
+    for i in range(len(snowflakes_fallen_coordinates)):
+        point = sd.get_point(snowflakes_fallen_coordinates[i][0], snowflakes_fallen_coordinates[i][1])
+
+        sd.snowflake(center=point, length=snowflakes_fallen_lengths[i], factor_a=snowflakes_fallen_factors_a_b_c[i][0],
+                     factor_b=snowflakes_fallen_factors_a_b_c[i][1], factor_c=snowflakes_fallen_factors_a_b_c[i][2])
+
+    sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
 
 sd.pause()
-
-# подсказка! для ускорения отрисовки можно
-#  - убрать clear_screen()
-#  - в начале рисования всех снежинок вызвать sd.start_drawing()
-#  - на старом месте снежинки отрисовать её же, но цветом sd.background_color
-#  - сдвинуть снежинку
-#  - отрисовать её цветом sd.COLOR_WHITE на новом месте
-#  - после отрисовки всех снежинок, перед sleep(), вызвать sd.finish_drawing()
-
-
-# 4) Усложненное задание (делать по желанию)
-# - сделать рандомные отклонения вправо/влево при каждом шаге
-# - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
-#   и добавлять новую снежинку
-# Результат решения см https://youtu.be/XBx0JtxHiLg
-
 
